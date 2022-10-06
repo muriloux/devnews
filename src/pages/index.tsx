06d1ea9d1ef781a9a491135047e0dcb010ab7161
muriloux <murilomelo.code@gmail.com>
2022-10-06 01:21:09 -0300

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import styles from "../styles/home.module.scss";
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <h1>
       Olá <span>Dev!</span>
     </h1>
   );
