bd0470ebf7dca18f9c66a76ef01a19c6bdcaf0a6
muriloux <murilomelo.code@gmail.com>
2022-10-11 06:17:12 -0300

    Adding a fake json-server and fetching in the index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
+interface IPost {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/posts").then((response) => {
+      response.json().then((data) => {
+        setPosts(data);
+      });
+    });
+  }, []);
+
   return (
-    <h1>
-      Olá <span>Dev!</span>
-    </h1>
+    <>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
