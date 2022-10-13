6b38a2e324e8f77938482c72eedd01822710dfd0
Naoki <murilomelo.code@gmail.com>
2022-10-13 17:17:05 -0300

    adding serverSideProps and statisProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import { useEffect, useState } from "react";
 
 interface IPost {
@@ -5,25 +6,40 @@ interface IPost {
   title: string;
 }
 
-export default function Home() {
-  const [posts, setPosts] = useState<IPost[]>([]);
+interface IHomeProps {
+  posts: IPost[];
+}
 
-  useEffect(() => {
-    fetch("http://localhost:3333/posts").then((response) => {
-      response.json().then((data) => {
-        setPosts(data);
-      });
-    });
-  }, []);
+export default function Home({ posts }: IHomeProps) {
+  // const [posts, setPosts] = useState<IPost[]>([]);
+
+  // useEffect(() => {
+  //  fetch("http://localhost:3333/posts").then((response) => {
+  //    response.json().then((data) => {
+  //    setPosts(data);
+  //   });
+  //  });
+  // }, []);
 
   return (
     <>
       <h1>Posts</h1>
       <ul>
-        {posts.map((post) => (
+        {posts.map((post: IPost) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,34 @@
-export default function Posts() {
-  return <h1>Posts</h1>;
+import { GetStaticProps } from "next";
+
+interface IPost {
+  id: string;
+  title: string;
 }
+
+interface IPostsProps {
+  posts: IPost[];
+}
+
+export default function Posts({ posts }: IPostsProps) {
+  return (
+    <>
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export const getStaticProps: GetStaticProps<IPostsProps> = async () => {
+  const response = await fetch("http://localhost:3333/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 5,
+  };
+};
